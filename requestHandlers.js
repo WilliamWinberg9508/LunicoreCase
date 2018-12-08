7437f12f2a5a5cc6d0298a7877c57ea3b291f1c5
William Winberg <William.winberg@gmx.com>
2018-12-08 19:19:15 +0100

    Changed program to work with given data.json file instead of my own

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,18 +1,14 @@
 var fs = require('fs');
-var employeeFile = './employeesData.json'
-var modelFile = './carmodelData.json'
+var JSONFile = 'Data.json'
 
 //Responds to a HTTP GET request, sends all employee parameters except sales
 //in JSON format as a response
 function getEmployees(response, postData) {
-    fs.readFile(employeeFile, 'utf8', function (err, fileData) {
+    fs.readFile(JSONFile, 'utf8', function (err, fileData) {
         if (err) {
             console.log(err);
         } else {
-            var employeeData=JSON.parse(fileData);
-            for (var i = 0; i < employeeData.length; i++) { 
-                delete employeeData[i].sales;
-            };
+            var employeeData=JSON.parse(fileData).carshop.employees;
             response.writeHead(200, {'Content-Type': 'application/json'});
             response.write(JSON.stringify(employeeData));
             response.end();
@@ -24,12 +20,13 @@ function getEmployees(response, postData) {
 //in JSON format as a response
 function getCarModels(response, postData) {
     console.log("Request handler 'carmodels' was called.");
-    fs.readFile(modelFile, 'utf8', function (err, fileData) {
+    fs.readFile(JSONFile, 'utf8', function (err, fileData) {
         if (err) {
             console.log(err);
         } else {
+            var carData=JSON.parse(fileData).carshop.carmodels;
             response.writeHead(200, {'Content-Type': 'application/json'});
-            response.write(fileData);
+            response.write(JSON.stringify(carData));
             response.end();
             console.log("Response for GET /carmodels sent");
         }
@@ -38,44 +35,56 @@ function getCarModels(response, postData) {
 //Responds to a HTTP GET request, sends all employee parameters and total sales
 //value in JSON format as a response
 function getTotalSales(response, postData) {
-    fs.readFile(employeeFile, 'utf8', function (err, fileData) {
+    fs.readFile(JSONFile, 'utf8', function (err, fileData) {
         if (err) {
             console.log(err);
         } else {
-            var employeeData=JSON.parse(fileData);
-            for (var i = 0; i < employeeData.length; i++) { 
-                var totalSales=0;
-                for (var j = 0; j < employeeData[i].sales.length; j++){
-                    totalSales += employeeData[i].sales[j].sale;
-                }
-                employeeData[i].sales=totalSales;
+            var Data=JSON.parse(fileData);
+            var employeeData=Data.carshop.employees;
+            var salesData=Data.carshop.sales;
+            var carData=Data.carshop.carmodels;
+            var totalSaleArray=[];
+            for(var i=0; i<employeeData.length; i++){
+                totalSaleArray[i]=0;
             }
-        };
-        response.writeHead(200, {'Content-Type': 'application/json'});
-        response.write(JSON.stringify(employeeData));
-        response.end();
-        console.log("Response for GET /total_sales sent");
-    });
-}
+            for (var i = 0; i < salesData.length; i++) { 
+                var sellerId=salesData[i].employee_id-1;
+                var carModelId=salesData[i].carmodel_id-1;
+                totalSaleArray[sellerId]=totalSaleArray[sellerId]+carData[carModelId].price;
+            };
+            for (var i = 0; i < employeeData.length; i++) {
+                employeeData[i]["sales"]= totalSaleArray[i];
+                }
+            }
+            response.writeHead(200, {'Content-Type': 'application/json'});
+            response.write(JSON.stringify(employeeData));
+            response.end();
+            console.log("Response for GET /total_sales sent");
+        });
+    };
+
 //Responds to a HTTP POST request containing a carmodel in JSON format 
 //and writes it to ./carmodelData.json
 function postCarModel(response, postData){
     console.log("Request handler 'post_carmodel' was called")
-    fs.readFile(modelFile, 'utf8', function readFileCallback(err, data){
+    fs.readFile(JSONFile, 'utf8', function readFileCallback(err, data){
         if (err){
             console.log(err);
         } else {   
-        existingModels = JSON.parse(data);
+        JSONData=JSON.parse(data);    
+        existingModels = JSONData.carshop.carmodels;
         newModel = JSON.parse(postData);
-        existingModels.push(newModel[0]);
-        allModelsJSON = JSON.stringify(existingModels, null, 2);
-        fs.writeFile(modelFile, allModelsJSON, 'utf8', function(err) {
+        newModel["id"] = existingModels.length+1;
+        existingModels.push(newModel);
+        JSONData.carshop["carmodels"]=existingModels;
+        updatedData = JSON.stringify(JSONData, null, 2);
+        fs.writeFile(JSONFile, updatedData, 'utf8', function(err) {
             if(err) {
                 console.log(err);
             } else {
                 console.log('carModelData.json was updated.');
                 response.writeHead(200, {'Content-Type': 'application/json'});
-                response.write(JSON.stringify(newModel[0]));
+                response.write(JSON.stringify(newModel));
                 response.end();
                 console.log("Response for POST /carmodels sent");
             }
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var url = require('url');
 
-function start(route, handle,port) {
+function start(route, handle, port) {
     http.createServer(function (request, response) {
         var pathname = url.parse(request.url).pathname;
         var postData ='';
@@ -15,10 +15,17 @@ function start(route, handle,port) {
                 route(handle, postPath, response, postData)
             });
         }
-        else{
+        else if(request.method=='GET') {
             console.log("GET Request for " + pathname + " received.");
             route(handle, pathname, response, postData);
         }
+        else {
+            console.log(request.method+" request received but is not defined for this endpoint");
+            response.writeHead(200, {"Content-Type": "text/plain"});
+            response.write(request.method+" request received but is not defined for this endpoint");
+            response.end();
+        }
+        
     }).listen(port);  
 console.log("Server has started.");
 }
